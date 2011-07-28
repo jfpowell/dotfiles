2579f24cc5e8e890c97d5b46ba3308f97230b8ca
Jonathan Powell <jpowell@jpowell.co.uk>
2011-07-28 21:26:36 +0100

    Add -rf to rm statements in dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
-rm $HOME/.git/
-rm $HOME/.gitconfig
-rm $HOME/.vim/
-rm $HOME/.vimrc
+rm -rf $HOME/.git/
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.vim/
+rm -rf $HOME/.vimrc
 ln -s $HOME/dotfiles/.git/ $HOME/.
 ln -s $HOME/dotfiles/.gitconfig $HOME/.
 ln -s $HOME/dotfiles/.vim/ $HOME/.
