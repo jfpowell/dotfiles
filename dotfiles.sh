87b0d231cbadce2ecce422763ce220ccb87976f3
Jonathan Powell <jpowell@jpowell.co.uk>
2011-07-28 18:46:22 +0100

    Basic shell script to install dotfiles.

---
+++ dotfiles.sh
@@ -0,0 +1,8 @@
+rm $HOME/.git/
+rm $HOME/.gitconfig
+rm $HOME/.vim/
+rm $HOME/.vimrc
+ln -s $HOME/dotfiles/.git/ $HOME/.
+ln -s $HOME/dotfiles/.gitconfig $HOME/.
+ln -s $HOME/dotfiles/.vim/ $HOME/.
+ln -s $HOME/dotfiles/.vimrc $HOME/.
