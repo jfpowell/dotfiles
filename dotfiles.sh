4bcffacab1683b07aa6367a299c1a1a3ead6205c
Jonathan Powell <jpowell@jpowell.co.uk>
2011-08-31 04:18:04 -0700

    Make dotfiles visible in dotfile directory, update dotfiles.sh to reflect this.  And add latex suite.

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,8 +2,8 @@ rm -rf $HOME/.gitconfig
 rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.bashrc
-ln -s $HOME/dotfiles/.gitconfig $HOME/.
-ln -s $HOME/dotfiles/.vim/ $HOME/.
-ln -s $HOME/dotfiles/.vimrc $HOME/.
-ln -s $HOME/dotfiles/.bashrc $HOME/.
+ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/vim/ $HOME/.vim
+ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
 
---
+++ vimrc
@@ -0,0 +1,30 @@
+"Disable vi incompatibilities
+set nocompatible
+
+"Line Numbers
+set number
+
+"Smart Identation
+set cindent
+set smartindent
+set autoindent
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+"Matching parentheses
+set showmatch
+
+set guioptions-=T
+
+set vb t_vb=
+
+set ruler
+
+set nohls
+
+set incsearch
+
+syntax on
+
+colorscheme molokai
