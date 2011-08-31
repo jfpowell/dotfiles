3e7e85933c67ef0c8cd3a069c36a46eb75e1d0c5
Jonathan Powell <jpowell@jpowell.co.uk>
2011-08-31 04:10:37 -0700

    Fix symlink for .bashrc

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,9 @@
-rm -rf $HOME/.git/
 rm -rf $HOME/.gitconfig
-rm -rf $HOME/.vim/
+rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.bashrc
-ln -s $HOME/dotfiles/.git/ $HOME/.
 ln -s $HOME/dotfiles/.gitconfig $HOME/.
 ln -s $HOME/dotfiles/.vim/ $HOME/.
 ln -s $HOME/dotfiles/.vimrc $HOME/.
-ln -s $HOME/dotfiles/.bashrc
+ln -s $HOME/dotfiles/.bashrc $HOME/.
+
