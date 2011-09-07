4e8c4ba8e960ec248d7089456fd858e52a6d7323
Jonathan Powell <jpowell@jpowell.co.uk>
2011-09-07 13:54:46 +0100

    Add .bash_aliases to dotfile.

---
+++ bash_aliases
@@ -0,0 +1,3 @@
+alias turing='ssh jp10@turing'
+alias keepass='mono /home/jpowell/Dropbox/Security/KeePassMono/KeePass.exe /home/jpowell/Dropbox/Security/KeyDatabase.kdbx'
+
--- dotfiles.sh
+++ dotfiles.sh
@@ -2,8 +2,9 @@ rm -rf $HOME/.gitconfig
 rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.bashrc
+rm -rf $HOME/.bash-aliases
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/vim/ $HOME/.vim
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
-
+ln -s $HOME/dotfiles/bash-aliases $HOME/.bash-aliases
