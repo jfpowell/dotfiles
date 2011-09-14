e5460e8f63229410f3a0ed661441452c7504b038
Jonathan Powell <jpowell@jpowell.co.uk>
2011-09-14 12:50:07 +0100

    dir_colors added to dotfiles (ARCH)

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,8 +3,10 @@ rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.bash-aliases
+rm -rf $HOME/.dir_colors
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/vim/ $HOME/.vim
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/bash-aliases $HOME/.bash-aliases
+ln -s $HOME/dotfiles/dir_colors $HOME/.dir_colors
