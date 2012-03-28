f7da61db78d0eff9476984d0c8f769dfd87f4f8d
Jonathan Powell <jpowell@jpowell.co.uk>
2012-03-28 09:52:31 +0100

    Fix bash_aliases misspelling.

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,8 @@
 rm -rf $HOME/.gitconfig
 rm -rf $HOME/.bashrc
-rm -rf $HOME/.bash-aliases
+rm -rf $HOME/.bash_aliases
 rm -rf $HOME/.dir_colors
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/bash-aliases $HOME/.bash-aliases
+ln -s $HOME/dotfiles/bash_aliases $HOME/.bash_aliases
 ln -s $HOME/dotfiles/dir_colors $HOME/.dir_colors
